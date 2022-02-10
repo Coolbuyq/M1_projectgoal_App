1cab1d26d91e82335ac84599e01d69364fbf3fab
Manjunadh521 <Manjunadh521@gmail.com>
2022-02-10 14:42:29 +0530

    commit

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	int details,attempt=2000,j,count=0,vaccstat=0;
+	int attempt=2000,j,count=0,vaccstat=0;
 		int result1=0,result2=0,result3=0,z=0;
 		reg();
 	long a;
@@ -88,7 +88,7 @@ while(attempt>0){
 	 goto predose2;}
 	 }}
 	 dose2:	
-	 
+
 	vaccstat=vacc_status(j);
 	if(vaccstat==1)
 	{   if(pd[j].vaccination_type==0||pd[j].vaccination_type==1||pd[j].vaccination_type==2)
