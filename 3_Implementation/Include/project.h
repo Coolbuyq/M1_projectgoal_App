1c180baf22005eb36211f28c436f84fe6f3a35c9
Manjunadh521 <Manjunadh521@gmail.com>
2022-02-11 00:36:20 +0530

    commit

--- 3_Implementation/Include/project.h
+++ 3_Implementation/Include/project.h
@@ -4,16 +4,17 @@
 #include<stdio.h>
 
 typedef struct person{  
-	int age;                    
-	long adhaar_no;             
-	long phone_no;              
-	int otp;                    
-	int vaccination_type;       
-	int vaccination_status;     
-	int vaccine_viles1;         
-	int vaccine_viles2;         
-	int vaccine_viles3;         
+	int age;                  
+	long adhaar_no;           
+	long phone_no;            
+	int otp;                  
+	int vaccination_type;     
+	int vaccination_status;   
+	int vaccine_viles1;      
+	int vaccine_viles2;       
+	int vaccine_viles3;       
 }person_details;
+
 person_details pd[2000];        
 
 int reg_status(long a);
@@ -26,7 +27,6 @@ int vacc_status(int j);
 int covaxin_vacc_viles(int i, int vstat);
 
 int covishield_vacc_viles(int i, int vstat);
-
 
 int sputnik_vacc_viles(int i, int vstat);
 #endif  
